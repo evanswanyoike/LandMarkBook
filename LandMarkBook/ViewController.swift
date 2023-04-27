bedba70be52f97a8bf5b80b9a0ab98f3bbd289ea
Evans Wanyoike <evanswanyoi@gmail.com>
2023-04-27 11:46:15 +0300

    Connecting the tableview

--- LandMarkBook/ViewController.swift
+++ LandMarkBook/ViewController.swift
@@ -13,24 +13,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
 
     @IBOutlet weak var tableView: UITableView!
+    var landMarkNames = [String]()
+    var landMarkImages = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+       
+        landMarkNames.append("Eiffel Tower")
+        landMarkNames.append("Great Wall of China")
+        landMarkNames.append("Pyramids of Giza")
+        landMarkNames.append("Sydney OPera House")
+        landMarkNames.append("Taj Mahal")
+        landMarkNames.append("New York - Times Square")
+        
+       
+        landMarkImages.append(UIImage(named: "Eiffel.jpg")!)
+        landMarkImages.append(UIImage(named: "great_Wall.jpg")!)
+        landMarkImages.append(UIImage(named: "pyramids.png")!)
+        landMarkImages.append(UIImage(named: "Sydney_Opera_House.jpg")!)
+        landMarkImages.append(UIImage(named: "taj_mahal.jpg")!)
+        landMarkImages.append(UIImage(named: "times_square.jpg")!)
+
+        
     }
     
     // numb
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return landMarkNames.count
     }
     // cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
 //        cell.textLabel?.text = "Evans"
         var content = cell.defaultContentConfiguration()
-        content.text = "Evans"
-        content.secondaryText = "Wanyoike"
+//        content.image = landMarkImages[indexPath.row]
+        content.text = landMarkNames[indexPath.row]
         cell.contentConfiguration = content
         return  cell
     }
