762570b85c4a682d6635bc99ca5ee77055c70325
Evans Wanyoike <evanswanyoi@gmail.com>
2023-04-27 10:35:48 +0300

    Swift TableView

--- LandMarkBook/ViewController.swift
+++ LandMarkBook/ViewController.swift
@@ -7,12 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    // numb
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    // cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+//        cell.textLabel?.text = "Evans"
+        var content = cell.defaultContentConfiguration()
+        content.text = "Evans"
+        content.secondaryText = "Wanyoike"
+        cell.contentConfiguration = content
+        return  cell
+    }
+    
 
 
 }
